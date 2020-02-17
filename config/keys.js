7271f60c00a13ae21685ad13aa923c5123e5b71c
MajorJon <jonnymajor1996@gmail.com>
2020-02-16 22:12:11 -0800

    Fixed keys.js

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    spotify: {
+      clientID: 'CLIENT_ID',
+      clientSecret: 'CLIENT_SECRET',
+    },
+    
+    mongodb: {
+      dbURI: 'DBURI'
+    },
+  
+    session: {
+      cookieKey: 'COOKIE_KEY'
+    }
+  };
